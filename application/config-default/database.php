e631bd9de415fbc4253054a43ddf57c565802564
restiaka <restiaka@gmail.com>
2011-09-27 11:59:58 +0700

    update file dalam folder config-default

--- application/config-default/database.php
+++ application/config-default/database.php
@@ -41,10 +41,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = '192.168.100.234';
-$db['default']['username'] = 'remote';
-$db['default']['password'] = 'remotepassword';
-$db['default']['database'] = 'erwin_140dev2';
+$db['default']['hostname'] = 'localhost';
+$db['default']['username'] = 'root';
+$db['default']['password'] = '';
+$db['default']['database'] = 'eliana_db';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
